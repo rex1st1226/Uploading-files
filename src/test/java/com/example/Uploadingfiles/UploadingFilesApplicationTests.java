7f36f96732f424f2a83258f638bc7e922aef0392
rex1st1226 <rex1st1226@126.com>
2021-09-07 16:24:17 +0800

    first commit

---
+++ src/test/java/com/example/Uploadingfiles/UploadingFilesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.Uploadingfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadingFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("hello world!");
+	}
+
+}
